e50dcb007d0d5b76c60a015272a4849d8eb70c62
Rodatz <b.p.rodatz@student.tue.nl>
2019-02-02 14:05:08 +0100

    graph creator

--- src/ExamEdge.java
+++ src/ExamEdge.java
@@ -1,10 +1,16 @@
 public class ExamEdge {
-    public int parent, target;
+    public int examA, examB;
     public int weight;
 
-    public ExamEdge(int parent, int target, int weight) {
-        this.parent = parent;
-        this.target = target;
+
+    public ExamEdge(int examA, int examB) {
+        this.examA = examA;
+        this.examB = examB;
+    }
+
+    public ExamEdge(int examA, int examB, int weight) {
+        this.examA = examA;
+        this.examB = examB;
         this.weight = weight;
     }
 }
--- src/GraphCreator.java
+++ src/GraphCreator.java
@@ -5,11 +5,24 @@ public class GraphCreator {
     public static Exam[] createGraph(Student[] students, int noExams) {
         Exam[] graph = new Exam[noExams];
         for (int i = 0; i < noExams; i++) {
-            graph[i] = new Exam(i);
+            graph[i] = new Exam(i, noExams);
         }
+
+        for (int i = 0; i < noExams; i++) {
+            for (int j = i; i < noExams; i++) {
+                ExamEdge edge = new ExamEdge(i, j);
+                graph[i].overlap[j] = edge;
+                graph[j].overlap[i] = edge;
+            }
+        }
+
+
         for (int i = 0; i < students.length; i++) {
             for (int j = 0; j < students[i].exams.length; j++) {
-
+                graph[students[i].exams[j]].students++;
+                for (int k = 0; k < students[i].exams.length; k++) {
+                    graph[students[i].exams[j]]; 
+                }
             }
         }
         return graph;
