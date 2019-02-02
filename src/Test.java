160c0df637f9124d98615db57f75fea30da560ea
Chess <34745596+Chessnl@users.noreply.github.com>
2019-02-02 12:17:23 +0100

    test123

--- src/Test.java
+++ src/Test.java
@@ -2,9 +2,7 @@
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hello world1232");
-		System.out.println("test123123123");
+		System.out.println("hello world");
 	}
 
 }
